1b59a46a03ce57558a85b522834d5a7cd87c3f1f
Madezz-o <55086670+Madezz-o@users.noreply.github.com>
2021-03-25 19:14:51 +0100

    Test 6 : Correction FIZZBUZZ
    Correction : Mettre en premier la condition du FIZZBUZZ pour éviter d'entrer dans les autres conditions

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,12 +2,12 @@ class FizzBuzz :
     def NombreMultiplicateur(nombre):
         if nombre <= 0:
             raise(errorException())
+        elif nombre % 3 == 0 and nombre % 5 == 0 :
+            return "FIZZBUZZ" 
         elif nombre % 3 == 0:
             return "FIZZ"
         elif nombre % 5 == 0:
             return "BUZZ"
-        elif nombre % 3 == 0 and nombre % 5 == 0 :
-            return "FIZZBUZZ" 
         return nombre
 
 
